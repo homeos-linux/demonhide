b81eedfdf4cd852ab1cf92143d67227c06e561e1
koyu <me@koyu.space>
2025-10-09 23:37:35 +0000

    Update source tarball to version 0.1.1

--- create-tar.sh
+++ create-tar.sh
@@ -2,7 +2,7 @@
 cargo vendor
 VERSION=$(grep -m 1 '^version = ' Cargo.toml | cut -d '"' -f2)
 tar czf "demonhide-$VERSION.tar.gz" --transform "s,^,demonhide-$VERSION/," \
-    Cargo.toml Cargo.lock src vendor .cargo
+    Cargo.toml Cargo.lock src vendor .cargo README.md LICENSE
 git add .
 git commit -m "Update source tarball to version $VERSION"
 git tag -d "demonhide-$VERSION-1" 2>/dev/null || true
